d3ba248eb85345526601b16d9a2d3af18fc5fc7a
Márcio Mingante <marciomvp17@gmail.com>
2024-03-08 11:21:48 -0300

    conclui req 3

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,12 +2,10 @@ const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
   const selectedAnimal = data.species
-    .filter((specie) => specie.name === animal)[0].residents
+    .filter(({ name }) => name === animal)[0].residents
     .every((resident) => resident.age >= age);
 
   return selectedAnimal;
 };
-
-// console.log(getAnimalsOlderThan('penguins', 14));
 
 module.exports = getAnimalsOlderThan;
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  // seu código aqui
+  const selectedEmployee = data.employees
+    .find(({ firstName, lastName }) => firstName === employeeName || lastName === employeeName);
+
+  if (selectedEmployee === undefined) return {};
+
+  return selectedEmployee;
 };
+console.log(getEmployeeByName());
 
 module.exports = getEmployeeByName;
