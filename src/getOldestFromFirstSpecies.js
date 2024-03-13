ba199e339676994e8892a718667deaf8b9c4e641
Márcio Mingante <marciomvp17@gmail.com>
2024-03-13 00:12:48 -0300

    conclui req 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const getOldest = data.employees
+    .find((employee) => employee.id === id);
+
+  const responsibilities = getOldest.responsibleFor[0];
+
+  const getAnimal = data.species
+    .find((specie) => specie.id === responsibilities);
+
+  const oldest = getAnimal.residents
+    .sort((a, b) => b.age - a.age);
+
+  return [oldest[0].name, oldest[0].sex, oldest[0].age];
 };
+
+// console.log(getOldestFromFirstSpecies('56d43ba3-a5a7-40f6-8dd7-cbb05082383f'));
 
 module.exports = getOldestFromFirstSpecies;
